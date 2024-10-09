5679fcda177c75334245f3ad5d6c0c0c796c04d1
Himanshi0827 <157987622+Himanshi0827@users.noreply.github.com>
2024-10-09 09:40:37 +0530

    changes

--- app.js
+++ app.js
@@ -15,14 +15,20 @@ import projectRouter from "./routes/projectRouter.js";
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
+// app.use(
+//   cors({
+//     origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    origin: ["https://himanshisingh-portfolio.netlify.app/", "https://himanshisinghdashboard.netlify.app/login"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
-
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
